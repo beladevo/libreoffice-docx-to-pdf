d0281a4e9dc9ae4d85d6de0fa8e0ff2256ffa892
Omri B <omri@easylend.co.il>
2024-06-14 09:38:47 +0300

    fix routes

--- app/routes.py
+++ app/routes.py
@@ -13,6 +13,9 @@ logger = logging.getLogger(__name__)
 
 @convert_bp.route("/convert", methods=["POST"])
 def convert():
+    temp_input_path = None
+    temp_output_path = None
+
     try:
         file = request.files.get("file")
 
@@ -20,46 +23,30 @@ def convert():
             logger.error("No file provided in the request.")
             return jsonify({"error": "No file provided"}), 400
 
-
-    try:
-        filename = secure_filename(file.filename)
-        with tempfile.NamedTemporaryFile(delete=False, suffix='.docx') as temp_input:
-            file.save(temp_input.name)
-            temp_input_path = temp_input.name
-
-        with ThreadPoolExecutor() as executor:
-            temp_output_path = executor.submit(convert_docx_to_pdf, temp_input_path).result()
-
-        return send_file(temp_output_path, as_attachment=True, download_name='converted.pdf')
-
         if file.filename.split(".")[-1].lower() != "docx":
             logger.error("Invalid file format provided.")
             return jsonify({"error": "Invalid file format"}), 400
 
-        temp_input_path = None
-        temp_output_path = None
+        filename = secure_filename(file.filename)
+        with tempfile.NamedTemporaryFile(delete=False, suffix=".docx") as temp_input:
+            file.save(temp_input.name)
+            temp_input_path = temp_input.name
 
-        try:
-            with tempfile.NamedTemporaryFile(
-                delete=False, suffix=".docx"
-            ) as temp_input:
-                file.save(temp_input.name)
-                temp_input_path = temp_input.name
+        with ThreadPoolExecutor() as executor:
+            temp_output_path = executor.submit(
+                convert_docx_to_pdf, temp_input_path
+            ).result()
 
-            temp_output_path = convert_docx_to_pdf(temp_input_path)
+        return send_file(
+            temp_output_path, as_attachment=True, download_name="converted.pdf"
+        )
 
-            return send_file(
-                temp_output_path, as_attachment=True, download_name="converted.pdf"
-              }
+    except Exception as e:
+        logger.error(f"Conversion error: {e}")
+        return jsonify({"error": str(e)}), 500
 
-        except Exception as e:
-            logger.error(f"Conversion error: {e}")
-            return jsonify({"error": str(e)}), 500
-
-    finally:
-        print("finally")
+    # finally:
     #     if temp_input_path and os.path.exists(temp_input_path):
     #         os.remove(temp_input_path)
     #     if temp_output_path and os.path.exists(temp_output_path):
     #         os.remove(temp_output_path)
-
