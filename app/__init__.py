d2d6924641427b15b2e77a737271e457244c4b48
Tung Le <sanbangtatca022015@gmail.com>
2025-12-20 10:12:15 +0700

    Update: add logging to console when run directly in dev mode

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,22 @@
 from flask import Flask
 from .routes import convert_bp
+import logging
+
+def setup_logging():
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s [%(process)d] [%(levelname)s] %(name)s: %(message)s",
+        force=True,  # ⬅ override Flask & Gunicorn
+    )
 
 def create_app():
+    setup_logging()  
+
     app = Flask(__name__)
-    
     app.register_blueprint(convert_bp)
 
-    @app.route('/health')
-    def health_check():
-        return 'OK', 200
+    @app.route("/health")
+    def health():
+        return "OK", 200
 
     return app
